fd30ec316310137789aa72102f3f046863c30e81
supersarahhh <sarahtran10@yahoo.comsarahtran10@yahoo.com>
2023-07-21 11:30:37 -0700

    middleware

--- settings.py
+++ settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'catcollector.urls'
